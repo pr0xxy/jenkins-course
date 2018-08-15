5fe2e47bb40fac6b37e146a4e8b010df81d95a8a
pr0xxy <james_notoma@hotmail.com>
2018-08-14 22:37:33 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/pr0xxy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/pr0xxy/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
